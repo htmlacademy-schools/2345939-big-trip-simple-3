54533ffcd51493283dc0e36eb4646067dc28a44a
BREWYM <basov0304@yandex.ru>
2023-06-07 18:06:55 +0300

    module 6.1

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import FilterView from './view/filter-view.js';
 import {render} from './framework/render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TripPointModel from './model/trip-point-model.js';
-import { mockInit } from './mock/utils.js';
+import {mockInit} from './mock/utils.js';
+import {offersByType} from './mock/const.js';
 
 const pageContainer = document.querySelector('.trip-events');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 
 const [tripPoints, destinations] = mockInit(5, 10);
+const tripPointsModel = new TripPointModel(tripPoints, destinations, offersByType);
 
-
-const tripPointsModel = new TripPointModel(tripPoints, destinations);
 const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel});
 render(new FilterView(), siteFilterElement);
 
 boardPresenter.init();
-
+//жоска модуль 6.1
